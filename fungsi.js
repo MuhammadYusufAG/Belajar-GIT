2540b5c9199473be0f4d7bb6977bcbef81e2d3a7
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-10-25 15:23:03 +0700

    ulang

--- fungsi.js
+++ fungsi.js
@@ -2,14 +2,10 @@ const add = (a,b) => a + b;
 console.log(add(1,2));
 
 const subtract = (a,b) => a - b;
-<<<<<<< HEAD
 console.log(subtract(5,3));
 
 const multiplyNumber = (a,b) => a * b;
-console.log(multiplyNumber(4,5));
+console.log(multi(4,5));
 
 const devide = (a,b) => a / b ;
 console.log(devide(10,2));
-=======
-console.log(subtract(5,3));
->>>>>>> parent of 4799e96 (menambahkan fitur perkalian)
