f5387e4a97fef7eb2308f712f26743512497b3c5
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-10-25 16:20:35 +0700

    memperbaiki multyply yg salah dan menambahkan math sin

--- fungsi.js
+++ fungsi.js
@@ -5,9 +5,13 @@ const subtract = (a,b) => a - b;
 console.log(subtract(5,3));
 
 const multiplyNumber = (a,b) => a * b;
-console.log(multi(4,5));
+console.log(multiplyNumber(4,5));
 
 const devide = (a,b) => a / b ;
 console.log(devide(10,2));
 
 console.log(add(5,5));
+
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(20));
+
