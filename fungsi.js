d7b8ae4e73c0d9c3333e780a89ae44b49f62ca68
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-10-25 15:20:26 +0700

    merefisi perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,10 +2,14 @@ const add = (a,b) => a + b;
 console.log(add(1,2));
 
 const subtract = (a,b) => a - b;
+<<<<<<< HEAD
 console.log(subtract(5,3));
 
-const multiply = (a,b) => a * b;
-console.log(multiply(4,5));
+const multiplyNumber = (a,b) => a * b;
+console.log(multiplyNumber(4,5));
 
 const devide = (a,b) => a / b ;
 console.log(devide(10,2));
+=======
+console.log(subtract(5,3));
+>>>>>>> parent of 4799e96 (menambahkan fitur perkalian)
